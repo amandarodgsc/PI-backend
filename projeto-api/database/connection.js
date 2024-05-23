e58cefd33a1689d2eb701d3fc5346ffad8fe5def
amandarodgsc <amandarodgsc30@gmail.com>
2024-05-23 12:19:35 -0300

    att mongodb

--- projeto-api/database/connection.js
+++ projeto-api/database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config(); // Carrega as variáveis do .env
 
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
@@ -8,19 +8,19 @@ const DB_NAME = process.env.DB_NAME;
 
 function DBConnect() {
     return new Promise((resolve, reject) => {
-        mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
-            .then(() => {
-                console.log("Conectado ao banco Mongo!");
-                resolve();
-            })
-            .catch(err => {
-                console.log("Erro ao conectar ao banco Mongo: ", err);
-                reject(err);
-            });
+        mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`, { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true 
+        })
+        .then(() => {
+            console.log("Conectado ao banco Mongo!");
+            resolve();
+        })
+        .catch(err => {
+            console.log("Erro ao conectar ao banco Mongo: ", err);
+            reject(err);
+        });
     });
 }
 
 module.exports = DBConnect;
-
-
-
--- projeto-api/index.js
+++ projeto-api/index.js
@@ -1,16 +1,20 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-const DBConnect = require('./database/connection')
+const DBConnect = require('./database/connection');
+
+// Chama a função para conectar ao banco de dados e inicia o servidor apenas depois
 DBConnect()
+    .then(() => {
+        app.use(express.json());
+        const routes = require('./routes/routes');
+        app.use(routes);
 
-app.use(express.json())
-
-const routes = require('./routes/routes')
-app.use(routes)
-
-
-app.listen(PORT, () => {
-    console.log(`Aplicação rodando na porta ${PORT}`)
-})
+        app.listen(PORT, () => {
+            console.log(`Aplicação rodando na porta ${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.error("Erro ao iniciar a aplicação:", error);
+    });
